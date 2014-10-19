37799ea2d922dbc8c5453736a315f731b24916dd
tesith <tesith@naver.com>
2014-10-20 03:03:25 +0900

    first commit

---
+++ SubtitleRenamer/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace SubtitleRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_search_listbox1 = new System.Windows.Forms.Button();
+            this.button_search_listbox2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button_reset_listbox1 = new System.Windows.Forms.Button();
+            this.button_reset_listbox2 = new System.Windows.Forms.Button();
+            this.button_up_listbox1 = new System.Windows.Forms.Button();
+            this.button_up_listbox2 = new System.Windows.Forms.Button();
+            this.button_progress = new System.Windows.Forms.Button();
+            this.Label_status = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Label_index = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(330, 256);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox_DragOver);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(346, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "자막파일 (영상파일이 여러개면 개수 같아야함)";
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(348, 25);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(330, 256);
+            this.listBox2.Sorted = true;
+            this.listBox2.TabIndex = 3;
+            this.listBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox2_DragDrop);
+            this.listBox2.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox_DragOver);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "영상파일 (1개 = 기준, 여러개 = 각각 매핑)";
+            // 
+            // button_search_listbox1
+            // 
+            this.button_search_listbox1.Location = new System.Drawing.Point(12, 287);
+            this.button_search_listbox1.Name = "button_search_listbox1";
+            this.button_search_listbox1.Size = new System.Drawing.Size(75, 23);
+            this.button_search_listbox1.TabIndex = 5;
+            this.button_search_listbox1.Text = "찾아보기...";
+            this.button_search_listbox1.UseVisualStyleBackColor = true;
+            // 
+            // button_search_listbox2
+            // 
+            this.button_search_listbox2.Location = new System.Drawing.Point(348, 287);
+            this.button_search_listbox2.Name = "button_search_listbox2";
+            this.button_search_listbox2.Size = new System.Drawing.Size(75, 23);
+            this.button_search_listbox2.TabIndex = 6;
+            this.button_search_listbox2.Text = "찾아보기...";
+            this.button_search_listbox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(203, 291);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "자동정렬";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(540, 291);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "자동정렬";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button_reset_listbox1
+            // 
+            this.button_reset_listbox1.Location = new System.Drawing.Point(281, 287);
+            this.button_reset_listbox1.Name = "button_reset_listbox1";
+            this.button_reset_listbox1.Size = new System.Drawing.Size(60, 23);
+            this.button_reset_listbox1.TabIndex = 9;
+            this.button_reset_listbox1.Text = "초기화";
+            this.button_reset_listbox1.UseVisualStyleBackColor = true;
+            this.button_reset_listbox1.Click += new System.EventHandler(this.button_reset_listbox1_Click);
+            // 
+            // button_reset_listbox2
+            // 
+            this.button_reset_listbox2.Location = new System.Drawing.Point(618, 287);
+            this.button_reset_listbox2.Name = "button_reset_listbox2";
+            this.button_reset_listbox2.Size = new System.Drawing.Size(60, 23);
+            this.button_reset_listbox2.TabIndex = 10;
+            this.button_reset_listbox2.Text = "초기화";
+            this.button_reset_listbox2.UseVisualStyleBackColor = true;
+            this.button_reset_listbox2.Click += new System.EventHandler(this.button_reset_listbox2_Click);
+            // 
+            // button_up_listbox1
+            // 
+            this.button_up_listbox1.Location = new System.Drawing.Point(93, 287);
+            this.button_up_listbox1.Name = "button_up_listbox1";
+            this.button_up_listbox1.Size = new System.Drawing.Size(23, 23);
+            this.button_up_listbox1.TabIndex = 11;
+            this.button_up_listbox1.Text = "↑";
+            this.button_up_listbox1.UseVisualStyleBackColor = true;
+            // 
+            // button_up_listbox2
+            // 
+            this.button_up_listbox2.Location = new System.Drawing.Point(429, 287);
+            this.button_up_listbox2.Name = "button_up_listbox2";
+            this.button_up_listbox2.Size = new System.Drawing.Size(23, 23);
+            this.button_up_listbox2.TabIndex = 12;
+            this.button_up_listbox2.Text = "↑";
+            this.button_up_listbox2.UseVisualStyleBackColor = true;
+            // 
+            // button_progress
+            // 
+            this.button_progress.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_progress.Location = new System.Drawing.Point(12, 322);
+            this.button_progress.Name = "button_progress";
+            this.button_progress.Size = new System.Drawing.Size(75, 23);
+            this.button_progress.TabIndex = 13;
+            this.button_progress.Text = "변환시작";
+            this.button_progress.UseVisualStyleBackColor = true;
+            this.button_progress.Click += new System.EventHandler(this.button_progress_Click);
+            // 
+            // Label_status
+            // 
+            this.Label_status.AutoSize = true;
+            this.Label_status.Location = new System.Drawing.Point(98, 322);
+            this.Label_status.Name = "Label_status";
+            this.Label_status.Size = new System.Drawing.Size(177, 12);
+            this.Label_status.TabIndex = 14;
+            this.Label_status.Text = "파일을 드래그 앤 드랍 해주세요";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(512, 322);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(166, 23);
+            this.progressBar.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 12);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Index of number : ";
+            // 
+            // Label_index
+            // 
+            this.Label_index.AutoSize = true;
+            this.Label_index.Location = new System.Drawing.Point(127, 358);
+            this.Label_index.Name = "Label_index";
+            this.Label_index.Size = new System.Drawing.Size(29, 12);
+            this.Label_index.TabIndex = 17;
+            this.Label_index.Text = "대기";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 382);
+            this.Controls.Add(this.Label_index);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.Label_status);
+            this.Controls.Add(this.button_progress);
+            this.Controls.Add(this.button_up_listbox2);
+            this.Controls.Add(this.button_up_listbox1);
+            this.Controls.Add(this.button_reset_listbox2);
+            this.Controls.Add(this.button_reset_listbox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button_search_listbox2);
+            this.Controls.Add(this.button_search_listbox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Subtitle Renamer v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_search_listbox1;
+        private System.Windows.Forms.Button button_search_listbox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button_reset_listbox1;
+        private System.Windows.Forms.Button button_reset_listbox2;
+        private System.Windows.Forms.Button button_up_listbox1;
+        private System.Windows.Forms.Button button_up_listbox2;
+        private System.Windows.Forms.Button button_progress;
+        private System.Windows.Forms.Label Label_status;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Label_index;
+    }
+}
+
---
+++ SubtitleRenamer/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;   //정규표현식
+
+namespace SubtitleRenamer
+{
+    public partial class Form1 : Form
+    {
+        FileInfo[] fileinfo1 = new FileInfo[0];
+        String[] strDroppedFiles1 = new String[0];
+
+        FileInfo[] fileinfo2 = new FileInfo[0];
+        String[] strDroppedFiles2 = new String[0];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            strDroppedFiles1 = (String[])e.Data.GetData(DataFormats.FileDrop, false);
+            FileInfo[] newFile = new FileInfo[strDroppedFiles1.Length + fileinfo1.Length];
+            
+            for (int i = 0; i < fileinfo1.Length; i++)
+            {
+                newFile[i] = fileinfo1[i];
+            }
+
+            for (int i = 0; i < strDroppedFiles1.Length; i++)
+            {
+                newFile[fileinfo1.Length + i] = new FileInfo(strDroppedFiles1[i]);
+                listBox1.Items.Add(newFile[fileinfo1.Length + i].Name);
+            }
+            fileinfo1 = newFile;
+
+            if (fileinfo1.Length == 1)
+            {
+                int index_of_num = fileinfo1[0].Name.IndexOf("[0-9]");
+                index_of_num = Regex.Match(fileinfo1[0].Name, @"\d\d").Index;
+                Label_index.Text = index_of_num.ToString();
+            }
+        }
+
+        private void listBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            strDroppedFiles2 = (String[])e.Data.GetData(DataFormats.FileDrop, false);
+            //fileinfo2 = new FileInfo[strDroppedFiles2.Length];
+            FileInfo[] newFile = new FileInfo[strDroppedFiles2.Length+fileinfo2.Length];
+            //FileName2 = new String[strDroppedFiles2.Length];
+            //FilePath2 = new String[strDroppedFiles2.Length];
+            for (int i = 0; i < fileinfo2.Length; i++)
+            {
+                newFile[i] = fileinfo2[i];
+            }
+
+            for (int i = 0; i < strDroppedFiles2.Length; i++)
+            {
+                //int LastIndex_of_file_boundary = strDroppedFiles2[i].LastIndexOf("\\");
+                //FilePath2[i] = strDroppedFiles2[i].Substring(0, LastIndex_of_file_boundary + 1);
+                //FileName2[i] = strDroppedFiles2[i].Substring(LastIndex_of_file_boundary + 1);
+                newFile[fileinfo2.Length + i] = new FileInfo(strDroppedFiles2[i]);
+                //listBox2.Items.Add(FileName2[i]);
+                listBox2.Items.Add(newFile[fileinfo2.Length + i].Name);
+            }
+            fileinfo2 = newFile;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                listBox1.Sorted = true;
+            }else{
+                listBox1.Sorted = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                listBox2.Sorted = true;
+            }
+            else
+            {
+                listBox2.Sorted = false;
+            }
+        }
+
+        private void button_reset_listbox1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            strDroppedFiles1 = new String[0];
+            fileinfo1 = new FileInfo[0];
+            Label_index.Text = "대기";
+        }
+
+        private void button_reset_listbox2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            strDroppedFiles2 = new String[0];
+            fileinfo2 = new FileInfo[0];
+            Label_index.Text = "대기";
+        }
+
+        private void button_progress_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count != 0 && listBox2.Items.Count != 0)
+            {
+                if (listBox1.Items.Count == 1)
+                {
+                    for (int i = 0; i < fileinfo2.Length; i++)
+                    {
+                        int index_of_num = Regex.Match(fileinfo1[0].Name, @"\d\d").Index;
+
+                    }
+
+                }
+                else if (listBox1.Items.Count == listBox2.Items.Count)
+                {
+                    listBox1.SetSelected(0, true);
+                    listBox2.SetSelected(0, true);
+
+                    for (int realCount = 0; realCount < fileinfo1.Length; realCount++)
+                    {
+                        bool esc = false;
+                        for (int i = 0; i < fileinfo1.Length && esc == false; i++)
+                        {
+                            if (listBox1.SelectedItem.ToString() == fileinfo1[i].Name)
+                            {
+                                for (int j = 0; j < fileinfo2.Length && esc == false; j++)
+                                {
+                                    if (listBox2.SelectedItem.ToString() == fileinfo2[j].Name)
+                                    {
+                                        int index_ext = fileinfo1[i].Name.LastIndexOf(".");
+                                        String substr = fileinfo1[i].Name.Substring(0, index_ext);
+                                        fileinfo2[j].MoveTo(fileinfo2[j].DirectoryName + "\\" + substr + fileinfo2[j].Extension);
+                                        esc = true;
+                                    }
+                                }
+                            }
+                        }
+                        if (!(listBox1.Items.Count == listBox1.SelectedIndex + 1))
+                        {
+                            listBox1.SetSelected(listBox1.SelectedIndex + 1, true);
+                            listBox1.SetSelected(listBox1.SelectedIndex, false);
+                            listBox2.SetSelected(listBox2.SelectedIndex + 1, true);
+                            listBox2.SetSelected(listBox2.SelectedIndex, false);
+                        }
+                    }
+                    listBox1.SetSelected(listBox1.SelectedIndex, false);
+                    listBox2.SetSelected(listBox2.SelectedIndex, false);
+                    Label_status.Text = "변환 완료";
+                }
+                else
+                {
+                    Label_status.Text = "영상파일 하나를 사용하거나, \n양쪽 파일의 개수가 일치해야 합니다";
+                }
+            }
+            else
+            {
+                Label_status.Text = "파일이 선택되지 않았습니다";
+            }
+        }
+    }
+}

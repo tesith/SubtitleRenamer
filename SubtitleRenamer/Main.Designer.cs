9c21947e1746b67db767983fc429cc27397a0074
tesith <tesith@naver.com>
2017-09-05 14:20:20 +0900

    update visible version

--- SubtitleRenamer/Main.Designer.cs
+++ SubtitleRenamer/Main.Designer.cs
@@ -244,7 +244,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "SubtitleRenamer";
-            this.Text = "Subtitle Renamer v1.3.1";
+            this.Text = "Subtitle Renamer v1.3.2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
